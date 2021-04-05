42777c6022997dd62c88754311ca1409ebae4346
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-04-05 17:11:20 -0400

    Update compiler.c

--- compiler.c
+++ compiler.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include "scanner.h"
 
-void scan(char code[1000]);
+void scan(char code[1000], Token buf_toks[1000]);
 
 int main(void) {
-    char code[1000] = "int a;";
-    scan(code);
+    char code[1000] = "'a";
+    Token tokens[strlen(code)];
+    scan(code, tokens);
+    char a[3];
     return 0;
 }
