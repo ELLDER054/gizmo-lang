ea3a543455a232db0488e89439731e728e47be40
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-04-27 17:54:05 -0400

    error info update

--- parser.c
+++ parser.c
@@ -5,6 +5,12 @@
 
 int ind = 0;
 
+void repeat_char(char c, int n, char string[1024]) {
+    for (int i = 0; i < n; i++) {
+        strncat(string, &c, 1);
+    }
+}
+
 void consume(Token tokens[1024], TokenType type, char err[25], char buffer[150]) {
     if (tokens[ind].type != type) {
         printf("%s", err);
@@ -59,13 +65,20 @@ Node* incomplete_var_declaration(int start, Token tokens[1024]) {
     char* id = expect_type(T_ID, tokens);
     if (id == NULL) {
         ind = start;
-        printf("Expected identifier after type\n");
+        char specifier[1024] = {'\0'};
+        repeat_char(' ', tokens[ind].col, specifier);
+        strncat(specifier, "^", 1);
+        printf("On line %d:\nExpected identifier after type\n%s\n%s\n", tokens[ind].lineno, tokens[ind].line, specifier);
         return NULL;
     }
     char* end = expect_type(T_SEMI_COLON, tokens);
     if (end == NULL) {
+        ind--;
+        char specifier[1024] = {'\0'};
+        repeat_char(' ', tokens[ind].col, specifier);
+        strncat(specifier, "^", 1);
+        printf("On line %d:\nExpected semi-colon after statement\n%s\n%s\n", tokens[ind].lineno, tokens[ind].line, specifier);
         ind = start;
-        printf("Expected semi-colon after statement\n");
         return NULL;
     }
     strcpy(node.name, id);
@@ -79,17 +92,17 @@ Node* statement(int start, Token tokens[1024]) {
     if (var != NULL) {
         return var;
     }
-    ind = start;
     return NULL;
 }
 
 void parse(Token tokens[1024], Node program[1024]) {
+    int stmt_c = 0;
     while (ind < tokslen(tokens)) {
         Node* stmt = statement(ind, tokens);
         if (stmt == NULL) {
             break;
         }
-        printf("success!\n");
+        program[stmt_c] = *stmt;
     }
     return;
 }
