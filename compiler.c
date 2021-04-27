32ac33ff2060ba7890667d5b465845efc38f8ac3
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-04-27 09:23:02 -0400

    added parser

--- compiler.c
+++ compiler.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include "scanner.h"
+#include "ast.h"
 
-void scan(char code[1000], Token buf_toks[1000]);
+void scan(char code[1024], Token buf_toks[1024]);
+void parse(Token tokens[1024], Node program[1024]);
 
 int main(void) {
-    char code[1000] = "'a";
+    char code[1024];
+    char c;
+
+    memset(code, 0, sizeof(code));
+
+    FILE* f = fopen("test.gizmo", "r");
+    fread(code, 1, sizeof(code), f);
     Token tokens[strlen(code)];
+    Node program[1028];
     scan(code, tokens);
-    char a[3];
+    parse(tokens, program);
+    fclose(f);
     return 0;
 }
