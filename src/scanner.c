161753087699f873ae35fc2b750fe26fcd3046db
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-05 17:30:04 -0400

    fixed readability

--- src/scanner.c
+++ src/scanner.c
@@ -92,26 +92,29 @@ int in_operators(char c) {
     return (c == '+') || (c == '-') || (c == '*') || (c == '/');
 }
 
-int split (const char *txt, char delim, char ***tokens) {
+int split(const char *txt, char delim, char ***tokens) {
     int *tklen, *t, count = 1;
-    char **arr, *p = (char *) txt;
+    char **arr, *p = (char*) txt;
 
-    while (*p != '\0') if (*p++ == delim) count += 1;
-    t = tklen = calloc (count, sizeof (int));
-    for (p = (char *) txt; *p != '\0'; p++) *p == delim ? *t++ : (*t)++;
-    *tokens = arr = malloc (count * sizeof (char *));
-    t = tklen;
-    p = *arr++ = calloc (*(t++) + 1, sizeof (char *));
-    while (*txt != '\0')
-    {
-        if (*txt == delim)
-        {
-            p = *arr++ = calloc (*(t++) + 1, sizeof (char *));
-            txt++;
-        }
-        else *p++ = *txt++;
+    while (*p != '\0') if (*p++ == delim) {
+        count++;
     }
-    free (tklen);
+    t = tklen = calloc(count, sizeof (int));
+    for (p = (char*) txt; *p != '\0'; p++) {
+        *p == delim ? *t++ : (*t)++;
+    }
+    *tokens = arr = malloc(count * sizeof(char*));
+    t = tklen;
+    p = *arr++ = calloc(*(t++) + 1, sizeof(char*));
+    while (*txt != '\0') {
+        if (*txt == delim) {
+            p = *arr++ = calloc(*(t++) + 1, sizeof(char*));
+            txt++;
+        } else {
+            *p++ = *txt++;
+        }
+    }
+    free(tklen);
     return count;
 }
 
