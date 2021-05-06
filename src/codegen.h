27237cf8d09721227df7936f4c82d5e48dbbbe96
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-06 11:23:51 -0400

    Update codegen.h

--- src/codegen.h
+++ src/codegen.h
@@ -1,6 +1,16 @@
 #ifndef codegen_h
 #define codegen_h
 
+typedef struct Item;
 
+typedef struct {
+    Item* next;
+    char* key;
+    char* val;
+} Item;
+
+typedef struct {
+    Item* begin;
+} Dict;
 
 #endif
