7f7c55ac52fb4818c513014bd7b925b950c88047
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-06 14:18:55 -0400

    fixed minor bugs

--- src/parser.c
+++ src/parser.c
@@ -159,8 +159,7 @@ void check_type(Node* left, Node* right, char* oper) {
                 char specifier[MAX_LINE_LEN] = "";
                 repeat_char(' ', tokens[ind - 1].col + strlen(tokens[ind - 1].value), specifier);
                 strncat(specifier, "^", 1);
-                printf("On line %d:\n
-                       integer on right side of expression\n%s\n%s\n", tokens[ind - 1].lineno, tokens[ind - 1].line, specifier);
+                printf("On line %d:\nExpected Integer on right side of expression\n%s\n%s\n", tokens[ind - 1].lineno, tokens[ind - 1].line, specifier);
                 exit(0);
             }
         }
