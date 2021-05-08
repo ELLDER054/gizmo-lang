d89a641ea2634b1535adcce2f33d9e639d37f53c
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-08 16:22:43 -0400

    Update compiler.c

--- src/compiler.c
+++ src/compiler.c
@@ -18,7 +18,8 @@ int main(int argc, char** argv) {
 
     memset(code, 0, sizeof(code));
     char file[sizeof(argv[1])] = {'K', ':', '/', '\0'};
-    strncpy(file, argv[1], sizeof(argv[1]));
+    int size = sizeof(argv[1]);
+    strncpy(file, argv[1], size);
     FILE* input_f = fopen(file, "r");
     fread(code, 1, sizeof(code), input_f);
     printf("code is %s in compiler.c\n", code);
