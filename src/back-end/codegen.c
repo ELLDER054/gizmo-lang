bad1c1201e1e0c566aee37d94a70f368609cddfa
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-08 18:04:04 -0400

    Update codegen.c

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+void generate_expression(Node* v) {
+    if (v->n_type == INTEGER_NODE) {
+        strcat(code, ((Integer_node*) v)->value);
+        return;
+    }
+}
 
 void generate(Node** ast, char* code) {
     for (int i = 0; i < sizeof(ast) / sizeof(Node*); i++) {
         Node* n = ast[i];
         if (n->n_type == VARIABLE_NODE) {
-            // add some IR to "code"
+            Var_declaration_node* v = (Var_declaration_node*) n;
+            char additional_code[1024] = "%%%s = ";
+            int size;
+            generate_expression(v->value, additional_code);
+            printf("code is %s\n", additional_code);
+            strncat(code, additional_code, strlen(additional_code));
         }
     }
 }
