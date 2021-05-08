b400b50c8c416cbfed6f5accc4604ef7eafb7e9e
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-08 15:42:08 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash -e
-clang -o gizmoc -O1 -g -fsanitize=address -fno-omit-frame-pointer src/*.c
+clang -o gizmoc -O1 -g -fsanitize=address -fno-omit-frame-pointer src/*.c src/front-end/*.c
 chmod +x gizmoc
 ./gizmoc $1 $2
