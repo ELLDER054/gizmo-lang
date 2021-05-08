954edca8bc82cb6bfb6f7da161f59a38e82ed17f
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-08 19:27:57 -0400

    Update compiler.c

--- src/compiler.c
+++ src/compiler.c
@@ -26,7 +26,7 @@ int main(int argc, char** argv) {
     memset(program, 0, sizeof(program));
     scan(code, tokens);
     parse(tokens, program);
-    printf("name: %s\n", ((Var_declaration_node*) program[0])->name);
+    printf("name: %s\n", ((Var_declaration_node*) program[0])->type);
     FILE* output_f = fopen(argv[2], "w");
     for (int i = 0; i < sizeof(program) / sizeof(Node*); i++) {
         if (NULL != program[i]) {
