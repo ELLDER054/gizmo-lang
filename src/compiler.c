bc9b53275cd67a1566d954f161a0e546842fd6c3
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-08 18:57:40 -0400

    Update compiler.c

--- src/compiler.c
+++ src/compiler.c
@@ -35,7 +35,7 @@ int main(int argc, char** argv) {
     }
     char output[1024];
     generate(program, output);
-    printf("%s\n", output);
+    printf("TEST: %s\n", output);
     fclose(input_f);
     fclose(output_f);
     for (int i = 0; i < sizeof(program) / sizeof(Node*); i++) {
