32175073dc8e6e6e4196827f35a339708d4f16ab
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-09 08:32:58 -0400

    changed strncat's to snprintf's

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -59,8 +59,8 @@ void generate(Node** ast, int size, char* code) {
         if (n->n_type == VAR_DECLARATION_NODE) {
             Var_declaration_node* v = (Var_declaration_node*) n;
             char additional_code[1024] = "%";
-            strncat(additional_code, v->name, strlen(v->name));
-            strncat(additional_code, " = ", 3);
+            snprintf(additional_code, sizeof(v->name), v->name);
+            snprintf(additional_code, 3, " = ");
             int size;
             generate_expression(v->value, additional_code);
             printf("code is %s\n", additional_code);
