ec6b860935687c78e8222cdf8281ba3ae6e009b4
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-09 18:27:59 -0400

    Update codegen.c

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -6,6 +6,18 @@
 
 int str_append_c = 0;
 
+char* generate_expression_asm(Node* n) {
+    char code[1024];
+    if (n->n_type == INTEGER_NODE) {
+        str_append_c += snprintf(code + str_append_c, 100, "%d", ((Integer_node*) n)->value);
+        return code;
+    } else if (n->n_type == ID_NODE) {
+        str_append_c += snprintf(code + str_append_c, 100, "%%%s", ((Identifier_node*) n)->name);
+        return code;
+    }
+    return generate_oper_asm();
+}
+
 void generate(Node** ast, int size, char* code) {
     for (int i = 0; i < size; i++) {
         Node* n = ast[i];
@@ -14,7 +26,7 @@ void generate(Node** ast, int size, char* code) {
         }
         if (n->n_type == VAR_DECLARATION_NODE) {
             Var_declaration_node* v = (Var_declaration_node*) n;
-            str_append_c += snprintf(code + str_append_c, 100, "%%%s = ", v->name);
+            str_append_c += snprintf(code + str_append_c, 100, "%%%s = %s", v->name, generate_expression_asm(v->value));
             printf("%s\n", code);
         }
     }
