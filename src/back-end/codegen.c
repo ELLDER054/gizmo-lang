43d71d591e4436d9d1d940cb29d5d133faf381e7
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-09 18:37:21 -0400

    made operator generation

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -6,6 +6,26 @@
 
 int str_append_c = 0;
 
+char* generate_oper_asm(char* oper, Node* left, Node* right) {
+    char ret[1024];
+    switch (*oper) {
+        case '+':
+            str_append_c += snprintf(ret + str_append_c, 100, "add %s, %s", (gnerate_expression_asm(left), gnerate_expression_asm(right));
+            return ret;
+        case '-':
+            str_append_c += snprintf(ret + str_append_c, 100, "sub %s, %s", (gnerate_expression_asm(left), gnerate_expression_asm(right));
+            return ret;
+        case '*':
+            str_append_c += snprintf(ret + str_append_c, 100, "mul %s, %s", (gnerate_expression_asm(left), gnerate_expression_asm(right));
+            return ret;
+        case '/':
+            str_append_c += snprintf(ret + str_append_c, 100, "div %s, %s", (gnerate_expression_asm(left), gnerate_expression_asm(right));
+            return ret;
+        default:
+            return '_';
+    }
+}
+
 char* generate_expression_asm(Node* n) {
     char code[1024];
     if (n->n_type == INTEGER_NODE) {
@@ -15,7 +35,8 @@ char* generate_expression_asm(Node* n) {
         str_append_c += snprintf(code + str_append_c, 100, "%%%s", ((Identifier_node*) n)->name);
         return code;
     }
-    return generate_oper_asm();
+    
+    return generate_oper_asm(((Operator_node*) n)->oper, ((Operator_node*) n)->left, ((Operator_node*) n)->right);
 }
 
 void generate(Node** ast, int size, char* code) {
