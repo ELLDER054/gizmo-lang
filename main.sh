7127d79e6f6f193c21bc6f5b57e580d8f1a4243e
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-10 10:27:30 -0400

    changed formatting

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash -e
 clang -o gizmoc -O1 -g -fsanitize=address -fno-omit-frame-pointer src/*.c src/front-end/*.c src/back-end/*.c
 chmod +x gizmoc
-./gizmoc $1 $2
+./gizmoc $1
+# llc -o $2 gizmo_llvm_ir
