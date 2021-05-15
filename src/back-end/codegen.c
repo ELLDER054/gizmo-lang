5f40e80895e2d8b18320bb4f83a0852564513d81
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-15 18:23:11 -0400

    restarting

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -5,74 +5,17 @@
 #include "../front-end/ast.h"
 
 #define MAX_BUF_LEN 1024
+
 /*reminder: printf code:
 
 declare i32 @printf(i8* noalias nocapture, ...)
 call i32 (i8*, ...)* @printf(i8* %msg, i32 12, i8 42)
 
 */
-void generate_expression_asm(Node* n, char* code);
 
 char* freeing[MAX_BUF_LEN];
 int j = 0;
 int var_c = 0;
-
-void find_expr(char* t, char o) {
-    switch (o) {
-        case '+':
-            strcat(t, "add");
-        case '-':
-            strcat(t, "sub");
-        case '*':
-            strcat(t, "mul");
-        case '/':
-            strcat(t, "sub");
-    }
-}
-
-void generate_oper_asm(char* oper, Node* left, Node* right, char* c) {
-    char* ret = malloc(MAX_BUF_LEN);
-    memset(ret, 0, MAX_BUF_LEN);
-    char* l = malloc(MAX_BUF_LEN);
-    memset(l, 0, MAX_BUF_LEN);
-    char* r = malloc(MAX_BUF_LEN);
-    memset(r, 0, MAX_BUF_LEN);
-    freeing[j++] = l;
-    freeing[j++] = r;
-    freeing[j++] = ret;
-    generate_expression_asm(left, l);
-    generate_expression_asm(right, r);
-    strcat(c, "%");
-    strcat(c, var_c);
-    strcat(c, " = ");
-    find_expr(ret, *oper);
-    strcat(c, ret);
-    strcat(c, l);
-    strcat(c, ", ");
-    strcat(c, r);
-    strcat(c, "\n");
-}
-
-void generate_expression_asm(Node* n, char* c) {
-    int expr_append_c = 0;
-    if (n->n_type == INTEGER_NODE) {
-        char integer[100];
-        snprintf(integer, 100, "add i32 %d, 0", ((Integer_node*) n)->value);
-        strcat(c, integer);
-        return;
-    } else if (n->n_type == STRING_NODE) {
-        char str[100];
-        snprintf(str, 100, "\"%s\"", ((String_node*) n)->value); /* strings are not generated correctly */
-        strcat(c, str);
-        return;
-    } else if (n->n_type == ID_NODE) {
-        strcat(c, "%");
-        strcat(c, ((Identifier_node*) n)->name);
-        return;
-    }
-    
-    generate_oper_asm(((Operator_node*) n)->oper, ((Operator_node*) n)->left, ((Operator_node*) n)->right, c);
-}
 
 char* types(char* t) {
     if (!strcmp(t, "int")) {
@@ -90,10 +33,11 @@ void generate(Node** ast, int size, char* code) {
         }
         if (n->n_type == VAR_DECLARATION_NODE) {
             Var_declaration_node* v = (Var_declaration_node*) n;
+            char* name = generate_expression_asm(v->value, code);
             strcat(code, "%");
             strcat(code, v->name);
             strcat(code, " = ");
-            generate_expression_asm(v->value, code);
+            strcat(code, name);
             strcat(code, "\n");
         }
     }
