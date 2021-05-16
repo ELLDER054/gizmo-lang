c7d0cdd4a3bee5ac038c2cc6c0c9d1be58aecb2d
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-16 14:28:07 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 clang -o gizmoc -O1 -g -fsanitize=address -fno-omit-frame-pointer src/*.c src/front-end/*.c src/back-end/*.c
 chmod +x gizmoc
 ./gizmoc $1 gizmo_llvm_ir.ll
-# llc -o $2 gizmo_llvm_ir.ll
+llc -o $2 gizmo_llvm_ir.ll
