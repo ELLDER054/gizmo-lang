b0730a2f22ffa6cf344bcbf50928b1374a08192c
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-16 14:31:10 -0400

    added assembler

--- main.sh
+++ main.sh
@@ -2,5 +2,6 @@
 clang -o gizmoc -O1 -g -fsanitize=address -fno-omit-frame-pointer src/*.c src/front-end/*.c src/back-end/*.c
 chmod +x gizmoc
 ./gizmoc $1 gizmo_llvm_ir.ll
-llc -o $2 gizmo_llvm_ir.ll
+llc -o asmbly gizmo_llvm_ir.ll
+llvm-as -o $2 asmbly
 chmod +x $2
