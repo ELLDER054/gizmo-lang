3f200d6a74b6cfdb0bcf9a702eb8525d69a21e29
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-17 16:45:26 -0400

    Update codegen.c

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -154,7 +154,7 @@ void generate(Node** ast, int size, char* code) {
             } else if (!strcmp(v->type, "real")) {
                 strcat(code, "%");
                 strcat(code, v->name);
-                strcat(code, " = load double*, double* ");
+                strcat(code, " = load double*, ");
                 strcat(code, var_name);
             }
             strcat(code, "\n");
