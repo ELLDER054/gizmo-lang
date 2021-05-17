c4406a21377bafea4defb37949e64ba16d27b426
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-17 16:30:04 -0400

    Update scanner.c

--- src/front-end/scanner.c
+++ src/front-end/scanner.c
@@ -172,6 +172,7 @@ void scan(char* code, Token* tokens) {
                 ch = code[++pos];
                 col++;
             }
+            printf("%c\n", next(code, pos));
             if (ch == '.' && isDigit(next(code, pos))) {
                 printf("Num: %s\n", num);
                 ch = code[++pos];
