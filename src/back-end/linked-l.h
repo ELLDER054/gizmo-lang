dfaf32dd1648ea5c11be035d9fea2ef82ff92270
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-19 15:01:51 -0400

    created custom list (easy and simple)

---
+++ src/back-end/linked-l.h
@@ -0,0 +1,9 @@
+#ifndef LINKEDL_H
+#define LINKEDL_H
+
+char** list_new();
+void list_add(char** list, char* val);
+int list_len(char** list);
+void list_end_use(char** list);
+
+#endif // LINKEDL_H
