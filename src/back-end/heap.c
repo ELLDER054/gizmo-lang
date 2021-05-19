66dfc85f007b8ca3a1fbfc33fe08c378e3b2e4c3
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-19 15:09:43 -0400

    optimized heap.c

---
+++ src/back-end/heap.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_ALLOCATIONS   1024
+
+static char** allocations;
+static int num_allocations;
+
+void heap_init(void) {
+    num_allocations = 0;
+    allocations = list_new();
+}
+
+void* heap_alloc(size_t size) {
+    void *m = malloc(size);
+    memset(m, 0, size);
+    list_add(allocations, m);
+    return m;
+}
+
+void heap_free_all(void) {
+    list_end_use(allocations);
+    num_allocations = 0;
+}
