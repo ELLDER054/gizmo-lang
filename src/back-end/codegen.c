fad30d09da6a863d9293dc047039ca0897f18395
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-20 19:26:30 -0400

    Update codegen.c

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -107,7 +107,7 @@ char* generate_expression_asm(Node* n, char* expr_type, char* c, char* end_size)
         char* str_name = heap_alloc(100);
         snprintf(str_name, 100, "%%%d", var_c);
         char* str_assignment = heap_alloc(100);
-        snprintf(str_assignment, 100, "%s = private unnamed_addr constant [%d x i8] c%s\n", str_llvm_name, strlen(str) + 2, str);
+        snprintf(str_assignment, 400, "%s = private unnamed_addr constant [%d x i8] c%s\n", str_llvm_name, strlen(str) + 2, str);
         insert(c, 0, 100, str_llvm_name);
         strcat(c, str_name);
         strcat(c, " = alloca i8*, align 8\nstore i8* getlementptr inbounds ([14 x i8], [14 x i8]* @.str.1, i64 0, i64 0), i8** ");
