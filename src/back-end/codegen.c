77c1035aff0885b020e1a3f8e7b3aea0e08e4cc4
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-20 19:45:16 -0400

    replaced code with c

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -108,7 +108,7 @@ char* generate_expression_asm(Node* n, char* expr_type, char* c, char* end_size)
         snprintf(str_name, 100, "%%%d", var_c);
         char* str_assignment = heap_alloc(100);
         snprintf(str_assignment, 400, "%s = private unnamed_addr constant [%d x i8] c\"%s\"\n", str_llvm_name, strlen(str) - 3, str);
-        insert(c, 0, strlen(code) - strlen(str_assignment), str_assignment);
+        insert(c, 0, strlen(c) - strlen(str_assignment), str_assignment);
         strcat(c, str_name);
         strcat(c, " = alloca i8*, align 8\nstore i8* getelementptr inbounds ([14 x i8], [14 x i8]* @.str.1, i64 0, i64 0), i8** ");
         strcat(c, str_name);
