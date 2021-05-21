b4b45ff12a554a6ca4e6ed067eff40155f6664fc
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-21 08:48:59 -0400

    removed terrible list

--- src/back-end/linked-l.h
+++
@@ -1,9 +0,0 @@
-#ifndef LINKEDL_H
-#define LINKEDL_H
-
-char** list_new();
-void list_add(char** list, char* val);
-int list_len(char** list);
-void list_end_use(char** list);
-
-#endif // LINKEDL_H
