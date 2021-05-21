76dfea069f8e5ecb66b0329b43178ca9fd4b4bba
gizmo-lang <76635411+ELLDER054@users.noreply.github.com>
2021-05-21 08:08:59 -0400

    Update codegen.c

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -192,7 +192,7 @@ void generate(Node** ast, int size, char* code) {
                 char* name = heap_alloc(100);
                 snprintf(name, 100, "%%%d", var_c++);
                 strcat(code, name);
-                strcat(code, " = load i8*, i8** ");
+                strcat(code, " = load i8*, i8* ");
                 strcat(code, write_arg_name);
                 strcat(code, ", align 8\n");
                 strcat(code, "call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str, i32 0, i32 0), i8* ");
