58ee1396050be18b17f52cb87e8fed2915581ada
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-05-22 17:51:31 -0400

    Update ast.c

--- src/front-end/ast.c
+++ src/front-end/ast.c
@@ -226,6 +226,7 @@ void free_node(Node* n) {
             free_Identifier_node((Identifier_node*) n);
             break;
         case FUNC_CALL_NODE:
+        case READ_NODE:
         case WRITE_NODE:
             free_Func_call_node((Func_call_node*) n);
             break;
