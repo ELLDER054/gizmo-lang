65695426a97b126d9038ccbdf1118e0d8bde8134
ELLDER054 <76635411+ELLDER054@users.noreply.github.com>
2021-06-11 17:10:37 -0400

    took out needless printing

--- src/back-end/codegen.c
+++ src/back-end/codegen.c
@@ -56,7 +56,6 @@ void insert(char* buf, int pos, int size, char* str) {
 }
 
 char* find_operation_asm(char* oper, char* t) {
-    printf("TYPE: %s, OPER: %s\n", t, oper);
     if (strcmp(t, "i32") == 0) {
         if (strcmp(oper, "+") == 0) {
             return "add";
