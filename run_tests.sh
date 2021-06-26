7727e3f21874cd24b3ce83315fbad5b65961ad74
Anthony DeRosa <5585976+freetink3r@users.noreply.github.com>
2021-06-26 15:51:59 -0400

    Added check for gizmo executable before running tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-python3 -m unittest
+if [ -f build/gizmo ]; then
+    echo "Running gizmo tests"
+    python3 -m unittest
+else
+    echo "ERROR: Failed to find build/gizmo needed for running tests"
+fi
